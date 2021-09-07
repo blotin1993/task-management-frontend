cea6976d4c6d08e176eb8a5215423a107af605c8
blotin1993 <blotin1993@users.noreply.github.com>
2021-09-07 20:56:04 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.e3b3790d62b3bae57feb1ae8223f5ddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7503e05bc52c42f86bce1c02b8280ea",
+    "url": "https://blotin1993.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "94c1251fac307ff9bcf1",
+    "url": "https://blotin1993.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0348c04385722c2c7712",
+    "url": "https://blotin1993.github.io/task-management-frontend/static/js/2.1e44b7d3.chunk.js"
+  },
+  {
+    "revision": "94c1251fac307ff9bcf1",
+    "url": "https://blotin1993.github.io/task-management-frontend/static/js/main.dfa6d01e.chunk.js"
+  },
+  {
+    "revision": "6ea2e24cc5a5949cadb7",
+    "url": "https://blotin1993.github.io/task-management-frontend/static/js/runtime~main.0f5f2924.js"
+  }
+]);
